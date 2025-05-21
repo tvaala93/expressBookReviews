f4c70892c5f06e7390eaa938559843f13f890e33
Ted Vaala <ted.vaala@gmail.com>
2025-05-21 14:57:07 -0400

    Completed tasks 2& 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,19 +12,44 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // Send JSON response with formatted friends data
+    res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    if (books[isbn]){
+        res.send(JSON.stringify(books[isbn],null,4));
+    }else{
+        res.status(404).json({message:`Unable to find ISBN: ${isbn}`})
+    }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
+    const author = req.params.author;
+
+    // Filter the books object for any book with the author
+    let book_keys = Object.keys(books);
+    //res.send(book_keys);
+    let authorsnamed = {};
+
+    for(let i = 0; i < book_keys.length; i++){
+        
+        if(books[book_keys[i]]["author"] === author){
+            authorsnamed[book_keys[i]] = books[book_keys[i]];            
+        }
+    }    
+
+    // Return true if any user with the same username is found, otherwise false
+    if (Object.keys(authorsnamed).length > 0) {
+        res.send(JSON.stringify(authorsnamed,null,4))
+    } else {
+        res.status(404).json({message:`Unable to find Author: ${author}`})
+    }
+
+    //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
