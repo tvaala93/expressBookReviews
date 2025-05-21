88805f09639436cc31e7de85410a1559b3d3a68a
Ted Vaala <ted.vaala@gmail.com>
2025-05-21 17:05:54 -0400

    Completed task 7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -33,14 +33,49 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Check if username or password is missing
+    if (username && password) {
+        // Authenticate user        
+        if (authenticatedUser(username, password)) {
+            // Generate JWT access token
+            let accessToken = jwt.sign({
+                data: password
+            }, 'access', { expiresIn: 60 * 60 });
+
+            // Store access token and username in session
+            req.session.authorization = {
+                accessToken, username
+            }
+            return res.status(200).send("User successfully logged in");
+        } else {
+            return res.status(208).json({ message: "Invalid Login. Check username and password" });
+        }
+    }else{
+        return res.status(404).json({ message: "Error logging in" });
+    }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // Verify user is logged in and has valid access token
+    if(req.session.authorization){
+        let token = req.session.authorization['accessToken'];
+
+        // Verify JWT token
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                req.user = user;
+                next(); // Proceed to the next middleware
+            } else {
+                return res.status(403).json({ message: "User not authenticated" });
+            }
+        });
+    }else {
+        return res.status(403).json({ message: "User not logged in" });
+    }
 });
 
 module.exports.authenticated = regd_users;
